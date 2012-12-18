3c76131c3902436bdb2cabea16ca8f485a7cbdf8
Nicholas Marson <nicmarson@gmail.com>
2012-12-18 14:50:11 -0800

    upload

---
+++ docroot/js/oo_conf_inline.js
@@ -0,0 +1,17 @@
+/*
+OnlineOpinion v5.5.6
+Released: 6/8/2012. Compiled 06/08/2012 02:39:28 PM -0500
+Branch: master 97ead7060650c0f058f39134c95280ef07ed56bc
+Components: Full
+The following code is Copyright 1998-2012 Opinionlab, Inc.  All rights reserved. Unauthorized use is prohibited. This product and other products of OpinionLab, Inc. are protected by U.S. Patent No. 6606581, 6421724, 6785717 B1 and other patents pending. http://www.opinionlab
+*/
+
+/* Inline configuration */
+  var oo_feedback = new OOo.Ocode({});
+  
+/*
+INLINE FEEDBACK LINK EXAMPLE
+<a href="javascript:void(0);" onClick="oo_feedback.show()"><img src="/onlineopinionV5/oo_icon.gif" border="0" title="Feedback"> Feedback</a>
+
+You can use this click event in as many elements as you'd like throughout a page/site
+*/
